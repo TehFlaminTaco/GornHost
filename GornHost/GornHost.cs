86fef6d1eae2ea529bd6c2f9f44de5bb86ebd661
William Lemon <william.lemon2@gmail.com>
2018-09-19 21:38:59 +1000

    Aaah

---
+++ GornHost/GornHost.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace GornHost
+{
+    class GornHost
+    {
+        public static Dictionary<int, Gorner> gorners = new Dictionary<int, Gorner>();
+        static void Main(string[] args)
+        {
+            IPAddress ipAd = IPAddress.Parse("127.0.0.1");
+            TcpListener server = new TcpListener(ipAd, 4676);
+
+            server.Start();
+
+            Console.WriteLine("Server running on port 4676");
+            Console.WriteLine("Local End point is : {0}", server.LocalEndpoint);
+            Console.WriteLine("Awaiting Connections...");
+            while (true)
+            {
+                Gorner gorner = new Gorner(server.AcceptTcpClient());
+                gorners[gorner.clientID] = gorner;
+                new Thread(new ThreadStart(gorner.Process)).Start();
+            }
+            //server.Stop();
+        }
+
+        public static int NextClientID()
+        {
+            int id = 0;
+            while (gorners.ContainsKey(id)){id++;}
+            return id;
+        }
+    }
+}
---
+++ GornHost/Gorner.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.IO;
+
+namespace GornHost
+{
+    class Gorner
+    {
+        TcpClient client;
+        NetworkStream clientStream;
+        public Dictionary<int, string> messagesToForward = new Dictionary<int, string>();
+        public int clientID;
+
+        public Gorner(TcpClient clnt)
+        {
+            this.client = clnt;
+            clientID = GornHost.NextClientID();
+            clientStream = client.GetStream();
+        }
+
+
+        public void Process()
+        {
+            StreamWriter sw = new StreamWriter(clientStream);
+            StreamReader sr = new StreamReader(sw.BaseStream);
+
+            sw.WriteLine("hello"); // Tell the client we can hear them.
+            sw.Flush();
+
+            string data;
+            try
+            {
+                while((data = sr.ReadLine()) != "end")
+                {
+                    foreach(KeyValuePair<int, Gorner> gorn in GornHost.gorners)
+                    {
+                        if (gorn.Key == clientID)
+                            continue;
+                        gorn.Value.messagesToForward[clientID] = data;
+                    }
+
+                    foreach(KeyValuePair<int, string> message in messagesToForward)
+                    {
+                        sw.WriteLine("{0}:{1}", message.Key, message.Value);
+                    }
+                    messagesToForward.Clear();
+                    sw.WriteLine("waiting");
+                    
+                }
+            }
+            finally
+            {
+                sw.Close();
+                GornHost.gorners.Remove(clientID);
+            }
+        }
+    }
+
+   
+}
